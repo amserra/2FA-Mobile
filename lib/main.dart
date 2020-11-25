b8a3bb170dd4c2de43c2e10b324d9758ed1dbf1b
Alexandre Serra <almateser@gmail.com>
2020-11-25 19:55:00 +0000

    Refactor design

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:pbkdf2_dart/pbkdf2_dart.dart';
 import 'package:crypto/crypto.dart';
@@ -14,16 +16,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '2FA Autheticator',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: '2FA Autheticator Home Page'),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme:
+              GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)),
+      home: MyHomePage(title: '2FA Autheticator'),
     );
   }
 }
@@ -73,32 +74,74 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double screen_width = MediaQuery.of(context).size.width;
+    double screen_height = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      backgroundColor: Color.fromRGBO(30, 30, 30, 1),
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(36, 36, 36, 1),
         title: Text(widget.title),
+        elevation: 50.0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Center(
+            heightFactor: 0.85,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You code is:',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  '$_counter',
+                  // style: Theme.of(context).textTheme.headline4,
+                  style: TextStyle(color: Colors.white, fontSize: 60),
+                ),
+                // Text(
+                //   '$_cameraScanResult',
+                //   style: TextStyle(color: Colors.white, fontSize: 30),
+                // ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              '$_cameraScanResult',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Positioned(
+              left: screen_width - 100,
+              top: screen_height - 500,
+              child: CircularCountDownTimer(
+                duration: 30,
+                // Controller to control (i.e Pause, Resume, Restart) the Countdown
+                // controller: _controller,
+                width: 80,
+                height: 80,
+                color: Colors.white,
+                fillColor: Color.fromRGBO(154, 205, 50, 0.8),
+                backgroundColor: null,
+                strokeWidth: 4.0,
+                textStyle: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                isReverse: true,
+                isReverseAnimation: true,
+                isTimerTextShown: true,
+                // Function which will execute when the Countdown Ends
+                onComplete: () {
+                  // Here, do whatever you want
+                  print('Countdown Ended');
+                },
+              ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: _readQRcode,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        label: Text('Scan new QR code'),
+        tooltip: 'Scan new QR code',
+        icon: Icon(Icons.qr_code),
+        backgroundColor: Color.fromRGBO(154, 205, 50, 0.8),
       ),
     );
   }
